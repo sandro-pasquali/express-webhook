85a1a04ac8ab73f84eff685215143aeb1e9e4c9f
Sandro Pasquali <spasquali@gmail.com>
2015-04-24 22:40:36 -0400

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app     = express();
-var port    = process.env.PORT || 8080;
+var port    = process.env.PORT || 8082;
 
 app.get('/', function(req, res) {
     res.send('Hello World!');  
